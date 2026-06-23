e825de9bcbb35f4aa317bd73fcc6b47f1e135a7c
Toshif Alam <toshif2005@gmail.com>
2026-06-23 14:16:46 +0530

    Add files via upload

--- pizza_sales_analysis.sql
+++ pizza_sales_analysis.sql
@@ -190,3 +190,18 @@ ON orders.order_id = order_details.order_id
 GROUP BY day_name
 ORDER BY revenue DESC
 LIMIT 1;
+
+
+-- Q15. What is the average order value?
+
+SELECT 
+    ROUND(AVG(order_value), 2) AS avg_order_value
+FROM
+    (SELECT 
+        order_details.order_id,
+            SUM(order_details.quantity * pizzas.price) AS order_value
+    FROM
+        order_details
+    JOIN pizzas ON order_details.pizza_id = pizzas.pizza_id
+    GROUP BY order_details.order_id) AS order_totals;
+    
